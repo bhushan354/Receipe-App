8335a8d5aeb193cd42d74d298db69aa4fee6784b
Haider <malikhaiderkhan81@gmail.com>
2024-01-29 22:50:33 +0500

    create recipe model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,9 @@
 class Food < ApplicationRecord
-    has_many :recipe_foods
-    belongs_to :user
-  
-    validates :name, presence: true
-    validates :measurement_unit, presence: true
-    validates :price, presence: true
-    validates :quantity, presence: true
+  has_many :recipe_foods
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
 end
