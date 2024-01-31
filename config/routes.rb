90ebdc0771c43c8ea2771ac11af9398bbdbf1ecb
bhushan <deshmukhumesh354@gmail.com>
2024-01-31 14:32:24 +0530

    Generate foods_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,19 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  
   get 'up' => 'rails/health#show', as: :rails_health_check
+  authenticated :user do
+    root 'foods#index', as: :authenticated_root
+  end
 
+  unauthenticated :user do
+    root 'home#index', as: :unauthenticated_root
+  end
+
+resources :foods, except: [:update]
   resources :recipes, only: %i[index show new create destroy]
   # Defines the root path route ("/")
-  root "recipes#index"
+  # root "recipes#index"
   # root "posts#index"
 end
