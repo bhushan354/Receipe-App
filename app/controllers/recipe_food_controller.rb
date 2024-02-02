1f326c7cd17dbaafc93a92bda85078257cd5193c
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-02 11:12:13 +0530

    Add recipe foods controller actions

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -6,18 +6,38 @@ class RecipeFoodsController < ApplicationController
     end
 
     def create
-            
+        @recipe_food = RecipeFood.new(recipe_food_params)
+        @recipe_food.recipe_id = params[:recipe_id]
+
+        if @recipe_food.save
+            redirect_to recipe_path(params[:recipe_id]), notice: 'food ingredient added successfully'
+        else
+            render :new
+        end
     end
 
     def destroy
-        
+        @recipe_food.destroy
+        redirect_to recipe_path(params[:recipe_id]), notice: 'food intergridient deleted succesfully'
     end
 
     def edit
+     
+    end
 
+    def update
+        if @recipe_food.update(recipe_food_params)
+            redirect_to recipe_path(params[:recipe_id]), notice: 'Food ingredient was successfully updated.'
+        else
+            render :edit
+        end
     end
 
     private
+
+    def recipe_food_params
+        params.require(:recipe_food).permit(:food_id, :quantity)
+    end
 
     def set_recipe_food
         @recipe_food = RecipeFood.find(params[:id])
