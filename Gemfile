854ab2b25380845bb70af35d61a608f33bd7abe3
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-02 22:52:16 +0530

    fix bug

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-ruby '3.2.2'
+ruby '3.0.2'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem 'rails', '~> 7.1.2'
@@ -48,10 +48,10 @@ gem 'bootsnap', require: false
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
+  gem 'capybara'
   gem 'debug', platforms: %i[mri windows]
   gem 'rails-controller-testing'
   gem 'rspec-rails'
-  gem 'capybara'
 end
 
 group :production do
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,20 +1,24 @@
 require 'rails_helper'
 RSpec.describe Recipe, type: :model do
-  let(:user) { User.create(name: 'Test User', email: 'test@example.com', password: 'password') }
+  let(:user) do
+    User.create(name: 'Test User', email: 'test@example.com', password: 'password', password_confirmation: 'password')
+  end
+
   it 'is valid with a name, preparation_time, cooking_time, and a user' do
-    recipe = Recipe.new(name: 'Delicious Dish', preparation_time: 30, public: true, cooking_time: 60, user:)
+    recipe = Recipe.new(name: 'Delicious Dish', preparation_time: 30, public: true, cooking_time: 60)
     expect(recipe).to be_valid
   end
+
   it 'is not valid without a name' do
-    recipe = Recipe.new(name: nil, preparation_time: 30, cooking_time: 60, user:)
+    recipe = Recipe.new(name: nil, preparation_time: 30, cooking_time: 60)
     expect(recipe).to_not be_valid
   end
   it 'is not valid without preparation_time' do
-    recipe = Recipe.new(name: 'Delicious Dish', preparation_time: nil, cooking_time: 60, user:)
+    recipe = Recipe.new(name: 'Delicious Dish', preparation_time: nil, cooking_time: 60)
     expect(recipe).to_not be_valid
   end
   it 'is not valid without cooking_time' do
-    recipe = Recipe.new(name: 'Delicious Dish', preparation_time: 30, cooking_time: nil, user:)
+    recipe = Recipe.new(name: 'Delicious Dish', preparation_time: 30, cooking_time: nil)
     expect(recipe).to_not be_valid
   end
   it 'is not valid without a user' do
@@ -22,7 +26,7 @@ RSpec.describe Recipe, type: :model do
     expect(recipe).to_not be_valid
   end
   it 'can have associated recipe foods' do
-    recipe = Recipe.new(name: 'Delicious Dish', preparation_time: 30, cooking_time: 60, user:)
+    recipe = Recipe.new(name: 'Delicious Dish', preparation_time: 30, cooking_time: 60)
     recipe.recipe_foods.build(quantity: 2, food_id: 1)
     expect(recipe.recipe_foods).to_not be_empty
   end
