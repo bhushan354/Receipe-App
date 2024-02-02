1c406f9290a598d721a4e26d05fbdcbea6ba0d5e
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-02 10:59:51 +0530

    Add controller actions

--- app/controllers/recipe_food_controller.rb
+++ app/controllers/recipe_food_controller.rb
@@ -1,2 +1,25 @@
 class RecipeFoodsController < ApplicationController
+    before_action :set_recipe_food, only: [:destroy, :edit, :update]
+
+    def new 
+        @recipe_food = RecipeFood.new
+    end
+
+    def create
+            
+    end
+
+    def destroy
+        
+    end
+
+    def edit
+
+    end
+
+    private
+
+    def set_recipe_food
+        @recipe_food = RecipeFood.find(params[:id])
+    end
 end
