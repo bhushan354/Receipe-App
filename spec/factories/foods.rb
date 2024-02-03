2e89e7ee2eaddc79cd93a249eeaa7d9db4843d51
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-03 07:52:06 +0530

    fix rubocop errors

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
-    factory :food do
-      name { 'Apple' }
-      measurement_unit { 'grams' }
-      price { 1.0 }
-      quantity { 100 }
-      association :user
-    end
+  factory :food do
+    name { 'Apple' }
+    measurement_unit { 'grams' }
+    price { 1.0 }
+    quantity { 100 }
+    association :user
+  end
 end
