9814d3fd6fce2e3e30f54761b88b9bc7bc0ccd89
bhushan354 <deshmukhumesh354@gmail.com>
2024-02-03 08:00:25 +0530

    Fix errors

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -5,17 +5,17 @@ RSpec.describe Food, type: :model do
   let(:user) { User.create(name: 'Binod', email: 'binod@email.com', password: 'password') }
 
   it 'is valid with valid attributes' do
-    food = Food.create(name: 'Apple', measurement_unit: 'grams', price: 1.0, user:, quantity: 100)
+    food = Food.create(name: 'Apple', measurement_unit: 'grams', price: 1.0, user: user, quantity: 100)
     expect(food).to be_valid
   end
 
   it 'is not valid without a name' do
-    food = Food.new(measurement_unit: 'grams', price: 0.5, user:)
+    food = Food.new(measurement_unit: 'grams', price: 0.5, user: user)
     expect(food).not_to be_valid
   end
 
   it 'is not valid with a negative price' do
-    food = Food.new(name: 'Apple', measurement_unit: 'grams', price: -0.5, user:)
+    food = Food.new(name: 'Apple', measurement_unit: 'grams', price: -0.5, user: user)
     expect(food).not_to be_valid
   end
 end
--- spec/models/recipe_food_spec.rb
+++ spec/models/recipe_food_spec.rb
@@ -5,27 +5,27 @@ RSpec.describe RecipeFood, type: :model do
 
   it 'is valid with a quantity, a food, and a recipe' do
     food = Food.create(name: 'Ingredient', measurement_unit: 'grams', price: 2.99)
-    recipe = Recipe.create(name: 'Test Recipe', preparation_time: 30, cooking_time: 60, user:)
-    recipe_food = RecipeFood.new(quantity: 2, food:, recipe:)
+    recipe = Recipe.create(name: 'Test Recipe', preparation_time: 30, cooking_time: 60, user: user)
+    recipe_food = RecipeFood.new(quantity: 2, food: food, recipe: recipe)
     expect(recipe_food).to be_valid
   end
 
   it 'is not valid without a quantity' do
     food = Food.create(name: 'Ingredient', measurement_unit: 'grams', price: 2.99)
-    recipe = Recipe.create(name: 'Test Recipe', preparation_time: 30, cooking_time: 60, user:)
-    recipe_food = RecipeFood.new(quantity: nil, food:, recipe:)
+    recipe = Recipe.create(name: 'Test Recipe', preparation_time: 30, cooking_time: 60, user: user)
+    recipe_food = RecipeFood.new(quantity: nil, food: food, recipe: recipe)
     expect(recipe_food).to_not be_valid
   end
 
   it 'is not valid without a food' do
-    recipe = Recipe.create(name: 'Test Recipe', preparation_time: 30, cooking_time: 60, user:)
-    recipe_food = RecipeFood.new(quantity: 2, food: nil, recipe:)
+    recipe = Recipe.create(name: 'Test Recipe', preparation_time: 30, cooking_time: 60, user: user)
+    recipe_food = RecipeFood.new(quantity: 2, food: nil, recipe: recipe)
     expect(recipe_food).to_not be_valid
   end
 
   it 'is not valid without a recipe' do
     food = Food.create(name: 'Ingredient', measurement_unit: 'grams', price: 2.99)
-    recipe_food = RecipeFood.new(quantity: 2, food:, recipe: nil)
+    recipe_food = RecipeFood.new(quantity: 2, food: food, recipe: nil)
     expect(recipe_food).to_not be_valid
   end
 end
